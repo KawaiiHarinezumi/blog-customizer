5f9be71482fdc081e6d35cc4c8a00d9951d4defb
Alina Sokolova <kawaiiharinezumi@yandex.ru>
2024-06-25 23:21:56 +0300

    first_review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,131 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+import { ArrowButton } from '../arrow-button/ArrowButton';
+import { Button } from '../button/Button'
+import { RadioGroup } from '../radio-group/RadioGroup';
+import { Select } from '../select/Select';
+import { Separator } from '../separator/Separator';
+import { Text } from '../text/Text';
+
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType
+} from '../../constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import React, { FormEvent, useRef, useState } from 'react';
 
-export const ArticleParamsForm = () => {
+type TArticleParamsProps = {
+	state: ArticleStateType;
+	onChange: (parameters: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ( { state, onChange }: TArticleParamsProps ) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [articleState, setArticleState] = useState({...state});
+	const rootRef = useRef<HTMLDivElement | null>(null);
+	
+	const handleClickForVisible = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleReset = () => {
+		setArticleState( {...defaultArticleState} );
+		onChange( {...defaultArticleState} );
+	};
+
+	const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+		setArticleState(articleState);
+		onChange(articleState);
+	};
+
+	useOutsideClickClose({
+		isOpen: isOpen,
+		rootRef: rootRef,
+		onChange: setIsOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={handleClickForVisible} isOpen={isOpen}/>
+			<aside 
+				className={clsx(styles.container, isOpen && {[styles.container_open]: isOpen })}
+				ref={rootRef}
+			>
+				<form 
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onClick={(evt) => evt.stopPropagation()}
+				>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+							Задайте параметры
+					</Text>
+					<Select
+						selected={articleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						onChange={(selected) => {
+							setArticleState( {
+								...articleState,
+								fontFamilyOption: selected,
+							});
+						}}
+					/>
+					<RadioGroup
+						name='font-size'
+						selected={articleState.fontSizeOption}
+						options={fontSizeOptions}
+						title='Размер шрифта'
+						onChange={ (selected) => {
+							setArticleState({
+								...articleState,
+								fontSizeOption: selected,
+							});
+						}}
+					/>
+					<Select
+						selected={articleState.fontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+						onChange={(selected) => {
+							setArticleState({
+								...articleState,
+								fontColor: selected,
+							});
+						}}
+					/>
+					<Separator />
+					<Select
+						selected={articleState.backgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+						onChange={(selected) => {
+							setArticleState({
+								...articleState,
+								backgroundColor: selected,
+							});
+						}}
+					/>
+					<Select
+						selected={articleState.contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						onChange={(selected) => {
+							setArticleState({
+								...articleState,
+								contentWidth: selected,
+							});
+						}}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={handleReset} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
